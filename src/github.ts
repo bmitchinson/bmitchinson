76db70184115d57110f200818b36471e862ae94a
Ben Mitchinson <mitchinson.dev@gmail.com>
2023-05-01 20:32:16 -0600

    Attempt 2: Correct readme file path

--- src/github.ts
+++ src/github.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 
+const readmeFile = 'README.md';
 const ghPrefixText = '### 📌 Current Projects';
 const ghSuffixText = `</br></br>🌐 https://mitchinson.dev
 
@@ -11,14 +12,14 @@ export const writeToReadme = (contents: string) => {
     const contentsToWrite =
         ghPrefixText + '\n' + contents + '\n' + ghSuffixText;
 
-    const existingReadme = fs.readFileSync('../README.md', 'utf8');
+    const existingReadme = fs.readFileSync(readmeFile, 'utf8');
 
     if (contentsToWrite === existingReadme) {
         console.log('✅ No changes to README.md, skipping write.');
         process.exit(0);
     }
 
-    fs.writeFile('../README.md', contentsToWrite, (err) => {
+    fs.writeFile(readmeFile, contentsToWrite, (err) => {
         if (err) {
             console.log('❌ Error writing readme', err);
         } else {
